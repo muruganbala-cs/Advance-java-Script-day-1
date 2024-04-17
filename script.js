b3bf3585988c2eac3545fdf241f5c49de9a49ba6
muruganbala-cs <balamaths.pmk@gamil.com>
2024-04-17 12:46:55 +0530

    updated

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout((a) => {
+  document.getElementById("container").innerHTML = "<b>10</b>";
+  setTimeout((a) => {
+    document.getElementById("container").innerHTML = "<b>9</b>";
+    setTimeout((a) => {
+      document.getElementById("container").innerHTML = "<b>8</b>";
+      setTimeout((a) => {
+        document.getElementById("container").innerHTML = "<b>7</b>";
+        setTimeout((a) => {
+          document.getElementById("container").innerHTML = "<b>6</b>";
+          setTimeout((a) => {
+            document.getElementById("container").innerHTML = "<b>5</b>";
+            setTimeout((a) => {
+              document.getElementById("container").innerHTML = "<b>4</b>";
+              setTimeout((a) => {
+                document.getElementById("container").innerHTML = "<b>3</b>";
+                setTimeout((a) => {
+                  document.getElementById("container").innerHTML = "<b>2</b>";
+                  setTimeout((a) => {
+                    document.getElementById("container").innerHTML = "<b>1</b>";
+                    setTimeout((a) => {
+                      document.getElementById("container").innerHTML =
+                        "<b>HAPPY INDIPENDANCE DAY</b>";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+},1000);
